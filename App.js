a2740e55d6d39a05f0971c33abb785c7db7bb94a
akshay-daundkar-14 <akshay.daundkar.net@gmail.com>
2026-05-02 09:06:05 +0530

    3. Laying the foundation. Render another component inside other component |  XSS attack | {}

--- App.js
+++ App.js
@@ -5,11 +5,23 @@ import  ReactDOM  from "react-dom/client";
 var jsxElement = <h1>Hello From JSX to Everyone!</h1>;
 
 
-// Functional Component - js function which returns JSX.
+// React element - JSX
+const title = <h1>Namaste Akshay - React Element</h1>
 
-const HeadingComponent = () =>{
-   return <h1>This is Heading component from Functional component</h1>
-}
+
+// React element - JSX
+const Title2 = () => (<h1>Namaste Akshay - React Functional Component</h1>);
+
+// Functional Component - js function which returns JSX.
+const HeadingComponent = () =>( 
+        <div className="heading">
+            This is Heading component from Functional component.
+            {title}
+            {Title2()} 
+            <Title2></Title2>
+            <Title2/>
+        </div>
+)
 
 // Component Composition - One component in another component
 
@@ -17,6 +29,7 @@ const BodyComponent = () =>{
    return (
     <div>
         <HeadingComponent/>
+        {HeadingComponent()}
         <h3>This is Body component from Functional component</h3>
     </div>
    )
